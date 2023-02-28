a11953f3146222e8b8a8853629565086dbb420a3
FangmingXie <fmxie1993@gmail.com>
2023-02-28 13:56:13 -0800

    MERFISH gene design v2

---
+++ 51.download_from_geo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# get from GEO
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P8NR.csv.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P14NR.csv.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P17NR.csv.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P21NR.csv.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P28NR.csv.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P38NR.csv.gz
+
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P28DR.csv.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P38DR.csv.gz
+
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE190nnn/GSE190940/suppl/GSE190940_P28DL.csv.gz
